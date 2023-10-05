1291e87e40416cede24ebe8f532be712f3f7e25f
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-10-06 01:48:21 +0300

    sript to prepare web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-apt update -y
-apt install -y nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>Nginx server test</p>
-  </body>
-</html>" | tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# Install Nginx if not already installed
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+# Create necessary directories and files
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Testing Nginx configuration" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link and update permissions
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config_content="server {
+    listen 80;
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}"
+
+echo "$config_content" | sudo tee /etc/nginx/sites-available/default
+
+# Restart Nginx
 sudo service nginx restart
