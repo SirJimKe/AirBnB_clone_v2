b7fdf917b2a10379d215edcf53ffcfebad830eb8
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-10-22 04:54:19 +0300

    hello hbnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,19 +2,19 @@
 """
 Starts a flask web application
 """
-
-
 from flask import Flask
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes=False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """route page"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
