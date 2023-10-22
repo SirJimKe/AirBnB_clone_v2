62c23b0a70f79d3c7a9d118e1a31bde8091095f8
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-10-22 05:44:54 +0300

    ✨✨✨ python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Starts a flask web app with C page
+"""
+from flask import Flask, escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """root page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """hbnb page"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    text = escape(text).replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text):
+    text = escape(text.replace('_', ' '))
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
